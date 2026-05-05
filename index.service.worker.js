5c052283fd894fb36e9f7c1c439154488fe599a2
DeanLemans <deanlemans@mailbox.org>
2026-05-05 22:30:09 +0000

    Deploying to gh-pages from @ DeanLemans/funny-game@66bb2f2884ea6dd115a9ae64cbd53dc08778e2e8 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1776966908|17142609';
+const CACHE_VERSION = '1778020197|17052070';
 /** @type {string} */
 const CACHE_PREFIX = 'rpg | NDG-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
